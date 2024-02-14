f6aabb2e90383a17af0f128bf0eb62031498a164
srs-sudeep <sudeepranjan@iitbhilai.ac.in>
2024-02-14 20:31:17 +0530

    fix:env

--- src/server.js
+++ src/server.js
@@ -1,3 +1,3 @@
-export const server = "https://vaidyaadmin.onrender.com/api/v2/product/get-all-products/api/v2";
-export const backend = "https://vaidyaadmin.onrender.com/api/v2/product/get-all-products/";
+export const server = "https://vaidyaadmin.onrender.com/api/v2";
+export const backend = "https://vaidyaadmin.onrender.com";
 
