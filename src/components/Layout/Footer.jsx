d9d4d7344ef54204ecba4eeaa49392efceb49e98
kruthi <sreekruthikaveri@gmail.com>
2024-02-14 14:47:48 +0530

    Footer colors and content modified

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -14,10 +14,10 @@ import {
 
 const Footer = () => {
   return (
-    <div className="bg-[#000] text-white">
-      <div className="md:flex md:justify-between md:items-center sm:px-12 px-4 bg-[#342ac8] py-7">
+    <div className="bg-[#49B9C8] text-black">
+      <div className="md:flex md:justify-between md:items-center sm:px-12 px-4 bg-[#FFFFFF] py-7">
         <h1 className="lg:text-4xl text-3xl md:mb-0 mb-6 lg:leading-normal font-semibold md:w-2/5">
-          <span className="text-[#56d879]">Subscribe</span> us for get news{" "}
+          <span className="text-[#49B9C8]">Subscribe</span> us for get news{" "}
           <br />
           events and offers
         </h1>
@@ -27,9 +27,9 @@ const Footer = () => {
             required
             placeholder="Enter your email..."
             className="text-gray-800
-                sm:w-72 w-full sm:mr-5 mr-1 lg:mb-0 mb-4 py-2.5 rounded px-2 focus:outline-none"
+                sm:w-72 w-full sm:mr-5 mr-1 lg:mb-0 mb-4 py-2.5 rounded px-2 focus:outline-none border border-black-500"
           />
-          <button className="bg-[#56d879] hover:bg-teal-500 duration-300 px-5 py-2.5 rounded-md text-whie md:w-auto w-full">
+          <button className="bg-[#49B9C8] hover:bg-teal-500 duration-300 px-5 py-2.5 rounded-md text-whie md:w-auto w-full">
             Submit
           </button>
         </div>
@@ -39,11 +39,11 @@ const Footer = () => {
           <img
             src="https://shopo.quomodothemes.website/assets/images/logo.svg"
             alt=""
-            style={{ filter: "brightness(0) invert(1)" }}
+            style={{ filter: "brightness(0) invert(0)" }}
           />
           <br />
-          <p>The home and elements needeed to create beatiful products.</p>
-          <div className="flex items-center mt-[15px]">
+          <p className="text-white">The home and elements needeed to create beatiful products.</p>
+          <div className="text-white flex items-center mt-[15px]">
             <AiFillFacebook size={25} className="cursor-pointer" />
             <AiOutlineTwitter
               size={25}
@@ -61,12 +61,12 @@ const Footer = () => {
         </ul>
 
         <ul className="text-center sm:text-start">
-          <h1 className="mb-1 font-semibold">Company</h1>
+          <h1 className="mb-2 font-semibold">Our Services</h1>
           {footerProductLinks.map((link,index) => (
             <li key={index}>
               <Link
-                className="text-gray-400 hover:text-teal-400 duration-300
-                   text-sm cursor-pointer leading-6"
+                className="text-white hover:text-teal-400 duration-300
+                   text-sm cursor-pointer leading-6 mb-2"
                 to={link.link}
               >
                 {link.name}
@@ -76,11 +76,11 @@ const Footer = () => {
         </ul>
 
         <ul className="text-center sm:text-start">
-          <h1 className="mb-1 font-semibold">Shop</h1>
+          <h1 className="mb-2 font-semibold">Explore More</h1>
           {footercompanyLinks.map((link,index) => (
             <li key={index}>
               <Link
-                className="text-gray-400 hover:text-teal-400 duration-300
+                className="text-white hover:text-teal-400 duration-300
                    text-sm cursor-pointer leading-6"
                 to={link.link}
               >
@@ -91,11 +91,11 @@ const Footer = () => {
         </ul>
 
         <ul className="text-center sm:text-start">
-          <h1 className="mb-1 font-semibold">Support</h1>
+          <h1 className="mb-2 font-semibold">Support</h1>
           {footerSupportLinks.map((link,index) => (
             <li key={index}>
               <Link
-                className="text-gray-400 hover:text-teal-400 duration-300
+                className="text-white hover:text-teal-400 duration-300
                    text-sm cursor-pointer leading-6"
                 to={link.link}
               >
@@ -108,7 +108,7 @@ const Footer = () => {
 
       <div
         className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10
-         text-center pt-2 text-gray-400 text-sm pb-8"
+         text-center pt-2 text-white text-sm pb-8"
       >
         <span>© 2020 Becodemy. All rights reserved.</span>
         <span>Terms · Privacy Policy</span>
