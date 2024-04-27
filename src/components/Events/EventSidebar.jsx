e320709b152adb6ba6f13b9ff1baaa2a06821ef3
kruthi <sreekruthikaveri@gmail.com>
2024-04-27 13:22:55 +0530

    Events added

--- src/components/Events/EventSidebar.jsx
+++ src/components/Events/EventSidebar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Carousel from './eventCarousel'
+import UserCard from './hcEventCard'
 
 const EventSidebar = () => {
   return (
@@ -8,6 +9,32 @@ const EventSidebar = () => {
     <br />
     <br />
     <p className="text-2xl font-semibold text-center">Ongoing <span className="bg-opacity rounded-md text-[#fff] bg-[#49B9C8] px-1 py-1">Events</span></p>
+    <br />
+    <br />
+    <UserCard className="w-[50vw]"/>
+    <br />
+    <br />
+    <UserCard className="w-[50vw]"/>
+    <br />
+    <br />
+
+    <p className="text-2xl font-semibold text-center">Past <span className="bg-opacity rounded-md text-[#fff] bg-[#49B9C8] px-1 py-1">Events</span></p>
+    <br />
+    <br />
+    <UserCard className="w-[50vw]"/>
+    <br />
+    <br />
+    <UserCard className="w-[50vw]"/>
+    <br />
+    <br />
+    <UserCard className="w-[50vw]"/>
+    <br />
+    <br />
+
+    <p className="text-2xl font-semibold text-center">Upcoming <span className="bg-opacity rounded-md text-[#fff] bg-[#49B9C8] px-1 py-1">Events</span></p>
+    <br />
+    <br />
+    <UserCard className="w-[50vw]"/>
     </>
     
   )
--- src/components/Events/hcEventCard.jsx
+++ src/components/Events/hcEventCard.jsx
@@ -5,117 +5,128 @@ import Button from '@mui/joy/Button';
 import Card from '@mui/joy/Card';
 import CardContent from '@mui/joy/CardContent';
 import Typography from '@mui/joy/Typography';
+import { styled } from '@mui/material/styles';
+import { purple } from '@mui/material/colors';
 import Sheet from '@mui/joy/Sheet';
+
+const ColorButton = styled(Button)(({ theme }) => ({
+  color: theme.palette.getContrastText(purple[500]),
+  backgroundColor: '#49B9C8',
+  '&:hover': {
+    backgroundColor: '#43A5B2'
+  },
+}));
 
 export default function UserCard() {
   return (
     <Box
       sx={{
         width: '100%',
-        position: 'relative',
-        overflow: { xs: 'auto', sm: 'initial' },
+        display: 'flex',
+        justifyContent: 'center', // Horizontally center the content
+        alignItems: 'center', // Vertically center the content
       }}
     >
       <Box
         sx={{
-          position: 'absolute',
-          display: 'block',
-          width: '1px',
-          bgcolor: 'warning.300',
-          left: '500px',
-          top: '-24px',
-          bottom: '-24px',
-          '&::before': {
-            top: '4px',
-            content: '"vertical"',
-            display: 'block',
-            position: 'absolute',
-            right: '0.5rem',
-            color: 'text.tertiary',
-            fontSize: 'sm',
-            fontWeight: 'lg',
-          },
-          '&::after': {
-            top: '4px',
-            content: '"horizontal"',
-            display: 'block',
-            position: 'absolute',
-            left: '0.5rem',
-            color: 'text.tertiary',
-            fontSize: 'sm',
-            fontWeight: 'lg',
-          },
-        }}
-      />
-      <Card
-        orientation="horizontal"
-        sx={{
-          width: '100%',
-          flexWrap: 'wrap',
-          [`& > *`]: {
-            '--stack-point': '500px',
-            minWidth:
-              'clamp(0px, (calc(var(--stack-point) - 2 * var(--Card-padding) - 2 * var(--variant-borderWidth, 0px)) + 1px - 100%) * 999, 100%)',
-          },
-          // make the card resizable for demo
-          overflow: 'auto',
-          resize: 'horizontal',
+          width: '80%', // Adjust the width of the container as needed
+          position: 'relative',
+          overflow: { xs: 'auto', sm: 'initial' },
         }}
       >
-        <AspectRatio flex ratio="1" maxHeight={182} sx={{ minWidth: 182 }}>
-          <img
-            src="bhilai_logo.png"
-            loading="lazy"
-            alt=""
-          />
-        </AspectRatio>
-        <CardContent>
-          <Typography fontSize="xl" fontWeight="lg">
-            Alex Morrison
-          </Typography>
-          <Typography level="body-sm" fontWeight="lg" textColor="text.tertiary">
-            Senior Journalist
-          </Typography>
-          <Sheet
-            sx={{
-              bgcolor: 'background.level1',
-              borderRadius: 'sm',
-              p: 1.5,
-              my: 1.5,
-              display: 'flex',
-              gap: 2,
-              '& > div': { flex: 1 },
-            }}
-          >
-            <div>
-              <Typography level="body-xs" fontWeight="lg">
-                Articles
-              </Typography>
-              <Typography fontWeight="lg">34</Typography>
-            </div>
-            <div>
-              <Typography level="body-xs" fontWeight="lg">
-                Followers
-              </Typography>
-              <Typography fontWeight="lg">980</Typography>
-            </div>
-            <div>
-              <Typography level="body-xs" fontWeight="lg">
-                Rating
-              </Typography>
-              <Typography fontWeight="lg">8.9</Typography>
-            </div>
-          </Sheet>
-          <Box sx={{ display: 'flex', gap: 1.5, '& > button': { flex: 1 } }}>
-            <Button variant="outlined" color="neutral">
-              Chat
-            </Button>
-            <Button variant="solid" color="primary">
-              Follow
-            </Button>
-          </Box>
-        </CardContent>
-      </Card>
+        <Box
+          sx={{
+            position: 'absolute',
+            display: 'block',
+            width: '1px',
+            bgcolor: 'warning.300',
+            left: '500px',
+            top: '-24px',
+            bottom: '-24px',
+            '&::before': {
+              top: '4px',
+              content: '"vertical"',
+              display: 'block',
+              position: 'absolute',
+              right: '0.5rem',
+              color: 'text.tertiary',
+              fontSize: 'sm',
+              fontWeight: 'lg',
+            },
+            '&::after': {
+              top: '4px',
+              content: '"horizontal"',
+              display: 'block',
+              position: 'absolute',
+              left: '0.5rem',
+              color: 'text.tertiary',
+              fontSize: 'sm',
+              fontWeight: 'lg',
+            },
+          }}
+        />
+        <Card
+          orientation="horizontal"
+          sx={{
+            width: '100%',
+            flexWrap: 'wrap',
+            [`& > *`]: {
+              minWidth: '182px', // Fixed minWidth for maintaining aspect ratio
+            },
+          }}
+        >
+          <AspectRatio flex ratio="1" maxHeight={182} sx={{ minWidth: 182 }}>
+            <img
+              src="bhilai_logo.png"
+              loading="lazy"
+              alt=""
+            />
+          </AspectRatio>
+          <CardContent>
+            <Typography fontSize="xl" fontWeight="lg">
+              Blood Donation Camp
+            </Typography>
+            <Typography level="body-sm" fontWeight="lg" textColor="text.tertiary">
+              ------------------
+            </Typography>
+            <Sheet
+              sx={{
+                bgcolor: 'background.level1',
+                borderRadius: 'sm',
+                p: 1.5,
+                my: 1.5,
+                display: 'flex',
+                gap: 2,
+                '& > div': { flex: 1 },
+              }}
+            >
+              <div>
+                <Typography level="body-xs" fontWeight="lg">
+                  Date
+                </Typography>
+                <Typography fontWeight="lg">01-05-2024</Typography>
+              </div>
+              <div>
+                <Typography level="body-xs" fontWeight="lg">
+                  Time
+                </Typography>
+                <Typography fontWeight="lg">9:30 A.M. - 4:30 P.M.</Typography>
+              </div>
+              <div>
+                <Typography level="body-xs" fontWeight="lg">
+                  Venue
+                </Typography>
+                <Typography fontWeight="lg">Sushrut Block</Typography>
+              </div>
+            </Sheet>
+            <Box sx={{ display: 'flex', gap: 1.5, '& > button': { flex: 1 } }}>
+              <ColorButton>
+                Register
+              </ColorButton>
+            </Box>
+          </CardContent>
+        </Card>
+      </Box>
     </Box>
   );
 }
